13f8d15fb8bde4b2276e3b5be9d96e26d8c94df6
Umair <akhtar.u@husky.neu.edu>
2018-02-08 19:42:36 -0500

    html & css files

---
+++ contact_me.php
@@ -0,0 +1,26 @@
+
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['msg'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['msg'];
+	
+// Create the email and send the message
+$to = 'magcommittee2013@gmail.com'; 
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+echo "Your mail has been sent successfully ! Thank you for your support";
+return true;			
+?>
